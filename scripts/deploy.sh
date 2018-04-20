74450bd4bba891c2fdc7585b618c23a523cffcb7
Stuart Mathews <stumathews@gmail.com>
2018-04-20 11:15:18 +0100

    Update deploy.sh
    added pip install --upgrade pip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,7 @@ export ECS_TASK=$IMAGE_NAME-task
 # install dependencies
 sudo apt-get install jq -y #install jq for json parsing
 sudo apt-get install gettext -y 
+pip install --upgrade pip
 pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 
