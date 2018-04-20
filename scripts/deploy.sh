e43ee3bb9b34b6eda207a1cc3cbee27ee031453a
Stuart Mathews <stuart.mathews@finbourne.com>
2018-04-20 16:32:48 +0100

    stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -40,7 +40,7 @@ else
     echo "Creating ECS Service $ECS_SERVICE ..."
     aws ecs create-service --cluster $AWS_ECS_CLUSTER_NAME --service-name $ECS_SERVICE --task-definition "$ECS_TASK:$TASK_REVISION" --desired-count 1 --region $AWS_DEFAULT_REGION > /dev/null #create service
 fi
-if [ "$(aws ecs list-tasks --service-name $ECS_SERVICE --region $AWS_DEFAULT_REGION | jq '.taskArns' | jq 'length')" -gt "0" ]; then
+if [ "$(aws ecs list-tasks --service-name $ECS_SERVICE --region $AWS_DEFAULT_REGION | jq '.taskArns' | jq 'length')" -gt 0 ]; then
     TEMP_ARN=$(aws ecs list-tasks --service-name $ECS_SERVICE --region $AWS_DEFAULT_REGION | jq '.taskArns[0]') # Get current running task ARN
     TASK_ARN="${TEMP_ARN%\"}" # strip double quotes
     TASK_ARN="${TASK_ARN#\"}" # strip double quotes
