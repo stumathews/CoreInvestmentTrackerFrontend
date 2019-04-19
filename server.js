0fb8c2b2211d6cb8ff5c856b142ea804e4e3710f
Stuart Mathews <stumathews@gmail.com>
2019-04-19 18:08:22 +0100

    refer to paths that actually exist doh

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/investment'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/investment/index.html'));
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 
