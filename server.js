15ccd05c74b7d3779ac6c567d8c3c1f01e27d63b
Stuart Mathews <stumathews@gmail.com>
2019-04-19 17:59:13 +0100

    change server.js to use static files

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var express = require('express');
-const app = express()
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/investment'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/investment/index.html'));
+});
+
+
 let port = process.env.PORT;
 
 if (port == null || port == "") {
-  port = 8000;
+	// Start the app by listening on the default Heroku port
+  port = 8080;
 }
-app.listen(port);
+app.listen(port,() => console.log(`API running on localhost:${port}`));
